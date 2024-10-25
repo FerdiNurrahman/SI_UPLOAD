9f28d71578065a4b9b5f023561e9de0c8c2462ea
FerdiNurrahman <ferdiart113@gmail.com>
2024-10-25 11:13:54 +0800

    a

--- resources/views/dashboard/board.blade.php
+++ resources/views/dashboard/board.blade.php
@@ -64,5 +64,36 @@
 
 </div>
 </main>
-
+<script>
+  var ctx = document.getElementById('categoryChart').getContext('2d');
+  var categoryChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+          labels: @json($categoryNames), // Menggunakan data dari controller
+          datasets: [{
+              label: 'Kategori Foto',
+              data: @json($categoryTotals), // Menggunakan data dari controller
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          responsive: true,
+          maintainAspectRatio: false,
+      }
+  });
+</script>
 @endsection
