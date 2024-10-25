13acfff8d0ffb2c53461ae1378aaf76118bd360f
FerdiNurrahman <ferdiart113@gmail.com>
2024-10-25 11:19:08 +0800

    more fiture

--- app/Http/Controllers/coba.php
+++ app/Http/Controllers/coba.php
@@ -1,22 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Photo;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class coba extends Controller
+class Coba extends Controller
 {
-    public function index(){
-        $categories = Photo::select('category', DB::raw('count(*) as total'))
-        ->groupBy('category')
-        ->get();
+    public function index()
+    {
+        // Ambil data pengguna dan jumlah foto yang diupload per pengguna
+        $uploadsByUser = Photo::select('account_id', DB::raw('count(*) as total'))
+            ->groupBy('account_id')
+            ->with('account') // Mengambil relasi akun dari model Photo
+            ->get();
 
-         // Konversi data untuk digunakan di Chart.js
-    $categoryNames = $categories->pluck('category')->toArray();
-    $categoryTotals = $categories->pluck('total')->toArray();
+        // Konversi data untuk digunakan di Chart.js
+        $userNames = $uploadsByUser->pluck('account.username')->toArray(); // Ambil username dari account
+        $userTotals = $uploadsByUser->pluck('total')->toArray(); // Ambil total upload dari pengguna
 
         $totalPhotos = Photo::count(); // Menghitung total foto yang ada di database
-        return view('dashboard/board', compact('totalPhotos','categoryNames', 'categoryTotals'));
+        return view('dashboard/board', compact('totalPhotos', 'userNames', 'userTotals'));
     }
 }
--- resources/views/dashboard/board.blade.php
+++ resources/views/dashboard/board.blade.php
@@ -1,91 +1,96 @@
 @extends('layout.main')
 @section('content')
-    
+
 <main class="app-content">
   <div class="app-title">
     <div>
       <h1><i class="bi bi-speedometer"></i> Dashboard</h1>
       <p>Sistem Informasi Upload Foto</p>
     </div>
-   
   </div>
-<div class="row">
-  <div class="col-md-6 col-lg-3">
-    <div class="widget-small primary coloured-icon"><i class="icon bi bi-people fs-1"></i>
-      <div class="info">
-        <h4>Users</h4>
-        <p><b>5</b></p>
+
+  <div class="row">
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small primary coloured-icon"><i class="icon bi bi-people fs-1"></i>
+        <div class="info">
+          <h4>Users</h4>
+          <p><b>5</b></p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="col-md-6 col-lg-3">
-    <div class="widget-small info coloured-icon"><i class="icon bi bi-heart fs-1"></i>
-      <div class="info">
-        <h4>Likes</h4>
-        <p><b>25</b></p>
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small info coloured-icon"><i class="icon bi bi-heart fs-1"></i>
+        <div class="info">
+          <h4>Likes</h4>
+          <p><b>25</b></p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="col-md-6 col-lg-3">
-    <div class="widget-small warning coloured-icon">
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small warning coloured-icon">
         <i class="icon bi bi-folder2 fs-1"></i>
         <div class="info">
-            <h4>Uploads</h4>
-            <p><b>{{ $totalPhotos }}</b></p> <!-- Menampilkan total upload foto -->
+          <h4>Uploads</h4>
+          <p><b>{{ $totalPhotos }}</b></p> <!-- Menampilkan total upload foto -->
         </div>
+      </div>
     </div>
-</div>      
-  <div class="col-md-6 col-lg-3">
-    <div class="widget-small danger coloured-icon"><i class="icon bi bi-star fs-1"></i>
-      <div class="info">
-        <h4>Stars</h4>
-        <p><b>500</b></p>
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small danger coloured-icon"><i class="icon bi bi-star fs-1"></i>
+        <div class="info">
+          <h4>Stars</h4>
+          <p><b>500</b></p>
+        </div>
       </div>
     </div>
   </div>
-</div>
-<div class="row">
-  <div class="col-md-6">
-    <div class="tile">
-      <h3 class="tile-title">Statistik Unggahan Mingguan</h3>
-      <div class="ratio ratio-16x9">
-        <div id="salesChart"></div>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6">
-    <div class="tile">
-      <h3 class="tile-title">Kategori Foto</h3>
-      <div class="ratio ratio-16x9">
-        <canvas id="categoryChart"></canvas> <!-- Ubah div ke canvas -->
-      </div>
-    </div>
-</div>
 
-</div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="tile">
+        <h3 class="tile-title">Statistik Unggahan Mingguan</h3>
+        <div class="ratio ratio-16x9">
+          <div id="salesChart"></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="tile">
+        <h3 class="tile-title">Unggahan per Pengguna</h3>
+        <div class="ratio ratio-16x9">
+          <canvas id="userUploadChart"></canvas> <!-- Tetap menggunakan canvas -->
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
+
 <script>
-  var ctx = document.getElementById('categoryChart').getContext('2d');
-  var categoryChart = new Chart(ctx, {
-      type: 'pie',
+  var ctx = document.getElementById('userUploadChart').getContext('2d');
+  var userUploadChart = new Chart(ctx, {
+      type: 'pie', // Tetap menggunakan pie chart
       data: {
-          labels: @json($categoryNames), // Menggunakan data dari controller
+          labels: @json($userNames), // Menggunakan data nama pengguna dari controller
           datasets: [{
-              label: 'Kategori Foto',
-              data: @json($categoryTotals), // Menggunakan data dari controller
+              label: 'Jumlah Foto yang Diunggah',
+              data: @json($userTotals), // Menggunakan data jumlah upload dari controller
               backgroundColor: [
                   'rgba(255, 99, 132, 0.2)',
                   'rgba(54, 162, 235, 0.2)',
                   'rgba(255, 206, 86, 0.2)',
                   'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)'
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(199, 199, 199, 0.2)'
               ],
               borderColor: [
                   'rgba(255, 99, 132, 1)',
                   'rgba(54, 162, 235, 1)',
                   'rgba(255, 206, 86, 1)',
                   'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)'
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(199, 199, 199, 1)'
               ],
               borderWidth: 1
           }]
